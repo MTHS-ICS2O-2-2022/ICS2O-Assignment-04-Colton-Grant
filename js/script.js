f1f2401ab9a74eb9eb7cf2e69548e89a530ce5ce
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-16 18:42:02 +0000

    added h

--- js/script.js
+++ js/script.js
@@ -3,3 +3,29 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function Calculate() {
+  // this function calculates the cost of pizza
+  
+  let subtotal = 0
+
+
+  const theSize= document.getElementById('size').value
+  
+  if(theSize == "small") {
+    subtotal = subtotal + 7
+  } else if(theSize == "medium") {
+    subtotal = subtotal + 11
+  } else if (theSize == "large") {
+    subtotal = subtotal + 17
+  }
+
+  // calculate tax
+  let tax = subtotal * 0.13
+
+  // calculate total
+  let total = subtotal + tax
+
+  // print total
+  document.getElementById('total').innerHTML = ("total: $") + total.toFixed(2)
+}
